38c03d9fc49c0873431f599d5c7144f494d1a734
Develop <>
2013-01-04 15:57:39 +0400

    add save/restore name/password

--- src/minecraftstarter/MainFrame.java
+++ src/minecraftstarter/MainFrame.java
@@ -1,21 +1,29 @@
 package minecraftstarter;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
-import java.net.URISyntaxException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Random;
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.PBEParameterSpec;
 
 /**
  *
  * @author Crimson
  */
 public class MainFrame extends javax.swing.JFrame {
-    
+
     private boolean def;
     private String S = SystemUtils.FILE_SEPARATOR;
     private String Q = "\"";
     private String ext = ".exe";
-    private String patch = SystemUtils.getJavaHome().getAbsolutePath();
+    private String patch = "";
 
     /**
      * Creates new form MainFrame
@@ -40,61 +48,112 @@ public class MainFrame extends javax.swing.JFrame {
         if (SystemUtils.OS_ARCH.contains("64")) {
             osArch.setSelectedIndex(1);
         }
-        
+        patch = buildMinecraftPatch();
+        dumpContents(patch);
+
         nameField.requestFocusInWindow();
         def = true;
-        
-    }
-    
-    public void launch() {
-        String java=Q+getJavaHome()+S+"java"+ext+Q;
-        String data=buildMinecraftPatch();
 
-        String exec = java+" -Xms"+xms.getText().trim()+"m -Xmx"+xmx.getText().trim()+"m -cp "+Q
-                + data+S+"bin"+S+"minecraft.jar;"
-                + data+S+"bin"+S+"lwjgl.jar;"
-                + data+S+"bin"+S+"lwjgl_util.jar;"
-                + data+S+"bin"+S+"jinput.jar;"
+    }
+
+    public void launch() {
+        String java = Q + getJavaHome() + S + "java" + ext + Q;
+
+        String exec = java + " -Xms" + xms.getText().trim() + "m -Xmx" + xmx.getText().trim() + "m -cp " + Q
+                + patch + S + "bin" + S + "minecraft.jar;"
+                + patch + S + "bin" + S + "lwjgl.jar;"
+                + patch + S + "bin" + S + "lwjgl_util.jar;"
+                + patch + S + "bin" + S + "jinput.jar;"
                 + Q
-                + " -Djava.library.path="+Q+data+S+"bin"+S+"natives"+Q
-                + " net.minecraft.client.Minecraft "+nameField.getText().trim();
-        
+                + " -Djava.library.path=" + Q + patch + S + "bin" + S + "natives" + Q
+                + " net.minecraft.client.Minecraft " + nameField.getText().trim();
+
         System.out.println(exec);
         try {
             Runtime.getRuntime().exec(exec);
+            writeUsername(patch);
         } catch (Exception ex) {
             System.err.println(ex.getLocalizedMessage());
         }
+
         System.exit(1);
-        
+
     }
-    
+
     public String getJavaHome() {
-        if (def) { return SystemUtils.getJavaHome().getAbsolutePath()+S+"bin"; } else {
+        if (def) {
+            return SystemUtils.getJavaHome().getAbsolutePath() + S + "bin";
+        } else {
             return buildPatch();
         }
     }
-    
+
     public String buildPatch() {
-        String p = System.getenv("ProgramFiles")+(((SystemUtils.OS_ARCH.contains("64")) && osArch.getSelectedIndex()==0)?" (x86)":"");
-        if (osArch.getSelectedIndex()==1) {
-            p=p.replace(" (x86)", "");
+        String p = System.getenv("ProgramFiles") + (((SystemUtils.OS_ARCH.contains("64")) && osArch.getSelectedIndex() == 0) ? " (x86)" : "");
+        if (osArch.getSelectedIndex() == 1) {
+            p = p.replace(" (x86)", "");
         }
-        String j = "Java"+S+"jre"+javaVersion.getSelectedItem().toString()+S+"bin";
-        return p+S+j;
+        String j = "Java" + S + "jre" + javaVersion.getSelectedItem().toString() + S + "bin";
+        return p + S + j;
     }
-    
-    public String buildMinecraftPatch() {
+
+    private String buildMinecraftPatch() {
         String dataFolder = "";
         if (new File("bin").exists() && new File("resources").exists()) {
             try {
-                dataFolder=new File(MainFrame.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath()).getParentFile().getPath().toString();
+                dataFolder = new File(MainFrame.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath()).getParentFile().getPath().toString();
             } catch (Exception ex) {
                 dataFolder = "";
                 System.err.println(ex.getLocalizedMessage());
             }
         }
-        return (dataFolder.isEmpty())?System.getenv("APPDATA")+S+".minecraft":dataFolder;
+        return (dataFolder.isEmpty()) ? System.getenv("APPDATA") + S + ".minecraft" : dataFolder;
+    }
+
+    private void dumpContents(String path) {
+        try {
+            File lastLogin = new File(path, "lastlogin");
+            Cipher cipher = getCipher(2, "passwordfile");
+            DataInputStream dIS;
+            if (cipher != null) {
+                dIS = new DataInputStream(new CipherInputStream(new FileInputStream(lastLogin), cipher));
+            } else {
+                dIS = new DataInputStream(new FileInputStream(lastLogin));
+            }
+            nameField.setText(dIS.readUTF());
+            passwordField.setText(dIS.readUTF());
+        } catch (Exception ex) {
+            System.err.println(ex.getLocalizedMessage());
+        }
+    }
+
+    private void writeUsername(String path) {
+        try {
+            File lastLogin = new File(path, "lastlogin");
+            Cipher cipher = getCipher(1, "passwordfile");
+            DataOutputStream dos;
+            if (cipher != null) {
+                dos = new DataOutputStream(new CipherOutputStream(new FileOutputStream(lastLogin), cipher));
+            } else {
+                dos = new DataOutputStream(new FileOutputStream(lastLogin));
+            }
+            dos.writeUTF(nameField.getText());
+            dos.writeUTF(rBox.isSelected() ? new String(passwordField.getPassword()) : "");
+            dos.close();
+        } catch (Exception ex) {
+            System.err.println(ex.getLocalizedMessage());
+        }
+    }
+
+    private static Cipher getCipher(int mode, String password) throws Exception {
+        Random random = new Random(0x29482c2L);
+        byte salt[] = new byte[8];
+        random.nextBytes(salt);
+        PBEParameterSpec pbeParamSpec = new PBEParameterSpec(salt, 5);
+        javax.crypto.SecretKey pbeKey = SecretKeyFactory.getInstance("PBEWithMD5AndDES").generateSecret(new PBEKeySpec(password.toCharArray()));
+        Cipher cipher = Cipher.getInstance("PBEWithMD5AndDES");
+        cipher.init(mode, pbeKey, pbeParamSpec);
+        return cipher;
     }
 
     /**
@@ -117,6 +176,9 @@ public class MainFrame extends javax.swing.JFrame {
         jLabel3 = new javax.swing.JLabel();
         xms = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
+        passwordField = new javax.swing.JPasswordField();
+        rBox = new javax.swing.JCheckBox();
+        jSeparator1 = new javax.swing.JSeparator();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Simple Minecraft Launcher");
@@ -131,10 +193,8 @@ public class MainFrame extends javax.swing.JFrame {
         });
 
         nameField.setFont(new java.awt.Font("Lucida Console", 0, 12)); // NOI18N
+        nameField.setToolTipText("Login");
         nameField.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyPressed(java.awt.event.KeyEvent evt) {
-                nameFieldKeyPressed(evt);
-            }
             public void keyReleased(java.awt.event.KeyEvent evt) {
                 nameFieldKeyReleased(evt);
             }
@@ -184,54 +244,70 @@ public class MainFrame extends javax.swing.JFrame {
         jLabel4.setFont(new java.awt.Font("Lucida Console", 0, 12)); // NOI18N
         jLabel4.setText("-Xms");
 
+        passwordField.setFont(new java.awt.Font("Lucida Console", 0, 12)); // NOI18N
+        passwordField.setToolTipText("Password ;)");
+
+        rBox.setToolTipText("Remember");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(osType, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(javaVersion, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(osArch, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(nameField, javax.swing.GroupLayout.PREFERRED_SIZE, 116, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(launchButton)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel4)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(xms, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel3)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(xmx, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(osType, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(javaVersion, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(osArch, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel4)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(xms, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(xmx, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(26, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(nameField, javax.swing.GroupLayout.PREFERRED_SIZE, 116, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(passwordField)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(rBox)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(launchButton)
+                        .addGap(4, 4, 4))))
+            .addComponent(jSeparator1)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(1, 1, 1)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(osArch)
-                            .addComponent(javaVersion)
-                            .addComponent(osType)))
-                    .addComponent(nameField, javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(launchButton, javax.swing.GroupLayout.DEFAULT_SIZE, 23, Short.MAX_VALUE)
-                    .addComponent(xmx, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(xms, javax.swing.GroupLayout.Alignment.TRAILING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(osType, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(javaVersion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(osArch, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel4)
+                    .addComponent(xms, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(xmx, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(nameField)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(passwordField)
+                    .addComponent(rBox, javax.swing.GroupLayout.DEFAULT_SIZE, 30, Short.MAX_VALUE)
+                    .addComponent(launchButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
 
@@ -239,27 +315,23 @@ public class MainFrame extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void osTypeItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_osTypeItemStateChanged
-        def=false;
+        def = false;
     }//GEN-LAST:event_osTypeItemStateChanged
 
     private void javaVersionItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_javaVersionItemStateChanged
-        def=false;
+        def = false;
     }//GEN-LAST:event_javaVersionItemStateChanged
 
     private void osArchItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_osArchItemStateChanged
-        def=false;
+        def = false;
     }//GEN-LAST:event_osArchItemStateChanged
 
     private void launchButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_launchButtonActionPerformed
         launch();
     }//GEN-LAST:event_launchButtonActionPerformed
 
-    private void nameFieldKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_nameFieldKeyPressed
-
-    }//GEN-LAST:event_nameFieldKeyPressed
-
     private void nameFieldKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_nameFieldKeyReleased
-        if (evt.getKeyCode()==java.awt.event.KeyEvent.VK_ENTER) {
+        if (evt.getKeyCode() == java.awt.event.KeyEvent.VK_ENTER) {
             launch();
         }
     }//GEN-LAST:event_nameFieldKeyReleased
@@ -304,11 +376,14 @@ public class MainFrame extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JSeparator jSeparator1;
     private javax.swing.JComboBox javaVersion;
     private javax.swing.JButton launchButton;
     private javax.swing.JTextField nameField;
     private javax.swing.JComboBox osArch;
     private javax.swing.JComboBox osType;
+    private javax.swing.JPasswordField passwordField;
+    private javax.swing.JCheckBox rBox;
     private javax.swing.JTextField xms;
     private javax.swing.JTextField xmx;
     // End of variables declaration//GEN-END:variables
